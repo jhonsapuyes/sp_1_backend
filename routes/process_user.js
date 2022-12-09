78c00f421474de3a7a8428ed2f14475c5388ae7c
jhon sapuyes <jhonsapuyes1986@gmail.com>
2022-12-08 19:46:58 -0500

    final backend

--- routes/process_user.js
+++ routes/process_user.js
@@ -9,6 +9,7 @@ const usuarios= require('../datos/sample.json')
 
 const querys= require('./sql/process_sql')
 
+//http://localhost:9000/api/deportes
 //http://localhost:9000/api/usuarios
 route.get('/:table',(req,res)=>{
     const sql_get='SELECT * from '+ req.params.table
@@ -26,10 +27,41 @@ route.get('/:table',(req,res)=>{
         //res.json(usuarios)
 })
 
+//http://localhost:9000/api/usuarios/nombre_equipo
+route.get('/:table/:nombre/p',(req,res)=>{
+    const sql_get='SELECT * from '+ req.params.table + ' WHERE mar_dep_id= "'+req.params.nombre+'" ORDER BY mar_fecha_event DESC LIMIT 3'
+    querys.conn.query(sql_get, (error,results) => {
+        if(error){
+            throw error;
+        }
+        if(results.length > 0){
+            res.json(results)
+        }
+        else{
+            res.json([{"respuesta":"not results"}])
+        }
+    })
+        //res.json(usuarios)
+})
+//http://localhost:9000/api/usuarios/nombre_equipo
+route.get('/:table/:nombre/e',(req,res)=>{
+    const sql_get='SELECT * from '+ req.params.table + ' WHERE mar_dep_id= "'+req.params.nombre+'"'
+    querys.conn.query(sql_get, (error,results) => {
+        if(error){
+            throw error;
+        }
+        if(results.length > 0){
+            res.json(results)
+        }
+        else{
+            res.json([{"respuesta":"not results"}])
+        }
+    })
+        //res.json(usuarios)
+})
+
 //http://localhost:9000/api/usuarios
 route.post('/:table',(req,res)=>{
-    const {nombre,password}= req.body
-    if(nombre&&password){
 
         const sql_post='INSERT INTO '+req.params.table+' SET ?'
         querys.conn.query(sql_post, [req.body], error => {
@@ -45,10 +77,8 @@ route.post('/:table',(req,res)=>{
         //const new_user={...req.body,id}
         //usuarios.push(new_user)
         //res.send(usuarios)
-    }
-    else{
-        res.send("wrong request")
-    }
+    
+
 })
 
 //http://localhost:9000/api/usuarios/ID/4
@@ -57,7 +87,7 @@ route.put('/:table/:field/:id',(req,res)=>{
     const tabla= req.params.table
     const campo_id= req.params.field
 
-    const sql_put= 'UPDATE '+tabla+' SET ?  WHERE '+campo_id+'= ?'
+    const sql_put= 'UPDATE '+tabla+' SET ? WHERE '+campo_id+'= ?'
     querys.conn.query(sql_put,[req.body,req.params.id], error => {
         if(error){
             throw error;
@@ -108,6 +138,23 @@ route.delete('/:table/:field/:id',(req,res)=>{
     //    }
     //})
     //res.json(usuarios)
+})
+
+//login   //http://localhost:9000/api/usuarios/usuario_1/clave_1
+route.get('/:table/:user/:pass',(req,res)=>{
+    const sql_get='SELECT * from '+ req.params.table + ' WHERE usu_nombre="' + req.params.user + '" AND usu_clave="' + req.params.pass +'"'
+    querys.conn.query(sql_get, (error,results,fields) => {
+        if(error){
+            throw error;
+        }
+        if(results.length > 0){
+            res.json(results)
+        }
+        else{
+            res.json([{"respuesta":"not results"}])
+        }
+    })
+        //res.json(usuarios)
 })
 
 module.exports= route;
